98330a67b86c3c92be73f4c9bdd5eff19326067d
Kieran O'Neill <kieran@mode.network>
2025-04-24 16:38:24 +0100

    fix: remove all locales from campaign

--- src/contentful.ts
+++ src/contentful.ts
@@ -53,3 +53,4 @@ fetchEntriesAndWriteToFile({ contentType: "stakingRewards", fileName: "stakingRe
 fetchEntriesAndWriteToFile({ contentType: "suggestions", fileName: "suggestions" });
 fetchEntriesAndWriteToFile({ contentType: "aiTerminalWhitelistedWallets", fileName: "aiTerminalWhitelistedWallets"});
 fetchEntriesAndWriteToFile({ contentType: "discoverPage", fileName: "discoverPage"});
+fetchEntriesAndWriteToFile({ contentType: "campaign", fileName: "campaign" });
